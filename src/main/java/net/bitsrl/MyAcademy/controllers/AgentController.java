586513a65875216f7065893f6c3250b2ff3c542c
raexell <josecarlosrobledo2903@gmail.com>
2020-01-28 12:45:22 +0100

    Modified AgentController

--- src/main/java/net/bitsrl/MyAcademy/controllers/AgentController.java
+++ src/main/java/net/bitsrl/MyAcademy/controllers/AgentController.java
@@ -1,12 +1,26 @@
 package net.bitsrl.MyAcademy.controllers;
 
+import net.bitsrl.MyAcademy.model.Agent;
+import net.bitsrl.MyAcademy.model.Course;
+import net.bitsrl.MyAcademy.services.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collection;
+
 @Controller
 public class AgentController {
-    @GetMapping("/employees")
-    public String getEmployees(){
-        return "employees";
+    AbstractService service;
+
+    @Autowired
+    public AgentController(AbstractService service){
+        this.service=service;
+    }
+    @GetMapping("/agents")
+    public String getAgent(){
+        Collection<Agent> agents = service.getAllAgent();
+        agents.forEach(System.out:: println);
+        return "agents/agentList";
     }
 }
