9944adfb2a6258de75c3766465b60c4ac39e64cc
raexell <josecarlosrobledo2903@gmail.com>
2020-01-28 15:46:04 +0100

    AgentController updated and creation html pages

--- src/main/java/net/bitsrl/MyAcademy/controllers/AgentController.java
+++ src/main/java/net/bitsrl/MyAcademy/controllers/AgentController.java
@@ -5,22 +5,39 @@ import net.bitsrl.MyAcademy.model.Course;
 import net.bitsrl.MyAcademy.services.AbstractService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
 
 @Controller
+@RequestMapping("/agents")
 public class AgentController {
-    AbstractService service;
+    private AbstractService service;
 
     @Autowired
     public AgentController(AbstractService service){
         this.service=service;
     }
-    @GetMapping("/agents")
-    public String getAgent(){
+    @GetMapping("/list")
+    public String listEmployees(Model model){
         Collection<Agent> agents = service.getAllAgent();
         agents.forEach(System.out:: println);
-        return "agents/agentList";
+        model.addAttribute("agents",agents);
+        return "/agents/agentList";
+    }
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model){
+        Agent agent = new Agent();
+        model.addAttribute("agent", agent);
+        return "/agents/agentForm";
+    }
+    @PostMapping("/save")
+    public String createAgent(@ModelAttribute("agent") Agent agent){
+        service.createAgent(agent);
+        return "redirect:/agents/list";
     }
 }
