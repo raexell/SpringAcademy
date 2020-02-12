e085c703f41d0527d1ca13629ce51ffc35e9c6e3
raexell <josecarlosrobledo2903@gmail.com>
2020-02-12 15:36:47 +0100

    Web service

--- src/main/java/net/bitsrl/MyAcademy/model/Enrollment.java
+++ src/main/java/net/bitsrl/MyAcademy/model/Enrollment.java
@@ -23,6 +23,7 @@ public class Enrollment {
         this.courseEdition= courseEdition;
     }
 
+    public Enrollment(){}
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name = "enrollment_id", nullable = false)
